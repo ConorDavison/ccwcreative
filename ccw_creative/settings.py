13f33bf5b0c1ac7c58fe1912e1901f729add2e51
ConorDavison <conorjamesdavison@hotmail.co.uk>
2021-11-09 19:40:20 +0000

    added checkout views and templates

--- ccw_creative/settings.py
+++ ccw_creative/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
+    # other
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -61,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ccw_creative.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,11 +81,17 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by all auth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
+
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
