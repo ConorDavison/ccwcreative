00530423a5b207face6918908d9b9bb39067ae7a
ConorDavison <conorjamesdavison@hotmail.co.uk>
2021-11-10 21:55:10 +0000

    stripe elements added

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Jmd0qIrsi5EmYBzdlYwlBbaV25PEUXs3DPcKcfpdJv0Y8WPsaBUktQy2BtaU49wSP87HrpnAgx7BMw3J36Lsskn00RLqpynSM',
+        "client_secret": "test client secret",
     }
 
     return render(request, template, context)
