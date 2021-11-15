5812757525e8dbbbff564636195ff41d3b4564ba
ConorDavison <conorjamesdavison@hotmail.co.uk>
2021-11-15 11:22:58 +0000

    re-exported stripe_wh_secret for webhook & added chaching in payment intent

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -7,8 +7,6 @@ from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
 
-# code taken directly from stripe. 
-
 
 @require_POST
 @csrf_exempt
@@ -25,7 +23,7 @@ def webhook(request):
 
     try:
         event = stripe.Webhook.construct_event(
-            payload, sig_header, wh_secret
+         payload, sig_header, wh_secret
         )
     except ValueError as e:
         # Invalid payload
@@ -35,6 +33,9 @@ def webhook(request):
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
+
+    print('success')
+    return HttpResponse(status=200)
 
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
