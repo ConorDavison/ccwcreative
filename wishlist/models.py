aec00a19e83c89ae9630d029d80862e9d3e58c7a
ConorDavison <conorjamesdavison@hotmail.co.uk>
2021-11-22 15:49:12 +0000

    added delete_from_wishlist & updated wishlist template

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from profiles.models import UserProfile
+from products.models import Product
 
 
 class Wishlist(models.Model):
     """ A model to link a wished product to the user """
-    user = models.OneToOneField(UserProfile, null=False, blank=False, on_delete=models.CASCADE, related_name='wishlist')
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE, null=False, blank=False, related_name='wishlist')
 
     def __str__(self):
         return f'Wishlist ({self.user})'
+
+
+class WishlistItem(models.Model):
+    """A many to many model to wishlist products"""
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE, null=False, blank=False, related_name='wishlist_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False, related_name='wishlist_products')
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
 
 from .models import Product, UserProfile, Wishlist, WishlistItem
 
 from django.contrib.auth.decorators import login_required
+
+from django.utils import timezone
+
+from django.contrib import messages
 
 
 @login_required
@@ -30,3 +34,58 @@ def wishlist(request):
             'wishlist': False,
         }
         return render(request, 'wishlist/wishlist.html', context)
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    """ A view to add a item in the Wishlist """
+    redirect_url = request.POST.get('redirect_url')
+
+    user = get_object_or_404(UserProfile, user=request.user)
+    wishlist = Wishlist.objects.get_or_create(user=user)
+    wishlist_user = wishlist[0]
+
+    product = Product.objects.get(pk=product_id)
+    if request.POST:
+        test = WishlistItem.objects.filter(wishlist=wishlist_user, product=product).exists()
+        if test:
+            messages.error(request, "Product already in your wishlist")
+            return redirect(redirect_url)
+
+        else:
+            added_item = WishlistItem(wishlist=wishlist_user, product=product, date_added=timezone.now())
+            added_item.save()
+            messages.success(request, "Product added to your wishlist")
+            return redirect(redirect_url)
+    else:
+        messages.error(request, "Click 'Add to wishlist' to add a item ")
+        return render(request, 'home/index.html')
+
+
+@login_required
+def delete_from_wishlist(request, product_id):
+    """ A view to delete a item in the wishlist"""
+
+    redirect_url = request.POST.get('redirect_url')
+
+    user = get_object_or_404(UserProfile, user=request.user)
+    wishlist = Wishlist.objects.get_or_create(user=user)
+    wishlist_user = wishlist[0]
+    if request.POST:
+        product = Product.objects.get(pk=product_id)
+
+        # look for product in the user's wishlistItem - returns true if it exists
+        test = WishlistItem.objects.filter(product=product).exists()
+
+        if test:
+            product = WishlistItem.objects.get(product=product)
+            product.delete()
+            messages.success(request, "Product removed from wishlist")
+            return redirect(redirect_url)
+
+        if test is None:
+            messages.error(request, "You can not delete a item that is not in your wishlist")
+            return redirect(redirect_url)
+    else:
+        messages.error(request, 'Item can only be deleted from your wishlist')
+        return render(request, 'home/index.html')
