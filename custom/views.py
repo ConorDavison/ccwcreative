1230de5fc7e70ba624de387ea27f363f1b0db6bb
ConorDavison <conorjamesdavison@hotmail.co.uk>
2021-11-24 15:53:40 +0000

    created contact model

--- custom/views.py
+++ custom/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import contact
+from django.http import HttpResponse
+from django.contrib import messages
 
 
 def contactForm(request):
+    if request.method == "POST":
+
+        contact = contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message') 
+        contact.name = name
+        contact.email = email
+        contact.subject = subject
+        contact.message = message
+        contact.save()
+        messages.success(request, 'Message recieved!')
     return render(request, 'custom/custom_form.html')
