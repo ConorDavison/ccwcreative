974eb642bac11346df391e0d5a5ca352ce95d32f
ConorDavison <conorjamesdavison@hotmail.co.uk>
2021-11-24 16:34:20 +0000

    adjusted margins on contact form

--- custom/views.py
+++ custom/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
 from .models import contact
-from django.http import HttpResponse
 from django.contrib import messages
 
 
 def contactForm(request):
     if request.method == "POST":
 
-        contact = contact()
+        Contact = contact()
         name = request.POST.get('name')
         email = request.POST.get('email')
         subject = request.POST.get('subject')
-        message = request.POST.get('message') 
+        message = request.POST.get('message')
         contact.name = name
         contact.email = email
         contact.subject = subject
         contact.message = message
-        contact.save()
+        Contact.save()
         messages.success(request, 'Message recieved!')
     return render(request, 'custom/custom_form.html')
