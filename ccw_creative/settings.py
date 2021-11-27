c55241abd469f8a817483038cca14d30d51b275c
ConorDavison <conorjamesdavison@hotmail.co.uk>
2021-11-27 13:02:28 +0000

    removed SECRET_KEY

--- ccw_creative/settings.py
+++ ccw_creative/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=e+*4=9v$m(zz+q@$-$0i(f0l1ak34bbtd3#&4$ye5-+o3-@1w'
+SECRET_KEY = os.environ.get('SECTRET_KEY', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ccwcreative.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://git.heroku.com/ccwcreative.git', 'localhost']
 
 
 # Application definition
