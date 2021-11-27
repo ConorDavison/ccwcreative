9be716ac3fffda65ff2b063ecb81c74c77a5497d
ConorDavison <conorjamesdavison@hotmail.co.uk>
2021-11-27 12:42:18 +0000

    disabled COLLECTSTATIC and added host

--- ccw_creative/settings.py
+++ ccw_creative/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-=e+*4=9v$m(zz+q@$-$0i(f0l1ak34bbtd3#&4$ye5-+o3-@1w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ccwcreative.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,12 +126,17 @@ WSGI_APPLICATION = 'ccw_creative.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
