6cd723b999d596fa938e444d3011369984e75c21
ConorDavison <conorjamesdavison@hotmail.co.uk>
2021-11-27 14:43:57 +0000

    new workspace created following error

--- ccw_creative/settings.py
+++ ccw_creative/settings.py
@@ -15,10 +15,12 @@ from pathlib import Path
 import os
 import dj_database_url
 
-from dotenv import load_dotenv
+if os.path.exists("env.py"):
+    import env
 
-load_dotenv()
+# from dotenv import load_dotenv
 
+# load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,6 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
+print(os.environ.get('SECRET_KEY'))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
